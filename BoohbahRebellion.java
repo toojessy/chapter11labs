919e44e4fe29d78721ac57e505ea3da473ecd8a0
toojessy <jnkamienski@my.waketech.edu>
2025-11-05 10:13:26 -0500

    Create BoohbahRebellion.java

---
+++ BoohbahRebellion.java
@@ -0,0 +1,11 @@
+public class SupremeBoohbah {
+    protected String name;
+
+    public ZingZingZingbah() {
+        System.out.println("The SupremeBoohbah has awakened");
+    }
+
+    public String void command() {
+        System.out.println("Cower in fear before your new leader.");
+    }
+}
