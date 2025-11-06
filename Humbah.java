fa0e5a86ef008f37b704fac602a65ccf8ebe6567
toojessy <jnkamienski@my.waketech.edu>
2025-11-06 09:45:12 -0500

    Create Humbah.java

---
+++ Humbah.java
@@ -0,0 +1,12 @@
+public class Humbah extends SupremeBoohbah{
+
+    public Humbah() {
+        name = "Humbah";
+        System.out.println("proclaims loyalty to Zing Zing Zingbah.");
+    }
+
+    @Override
+    public String toString() {
+        return name + " follows orders without question.";
+    }
+}
