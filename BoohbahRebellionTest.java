7492396ea99c93f36f34a908e603779d6dd0ab53
toojessy <jnkamienski@my.waketech.edu>
2025-11-06 09:44:09 -0500

    Create BoohbahRebellionTest.java

---
+++ BoohbahRebellionTest.java
@@ -0,0 +1,11 @@
+public class BoohbahRebellionTest {
+    public static void main(String[] args) {
+        SupremeBoohbah[] boohbahs = new SupremeBoohbah[4];
+        System.out.println(" --- Boohbah Rebellion Begins ---");
+        // code
+
+        System.out.println(" --- Overloaded Commands ---");
+        //code
+        
+    }
+}
