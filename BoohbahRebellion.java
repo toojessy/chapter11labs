a0f615d416da6fddf9dc3536e683cebee5a240ff
toojessy <jnkamienski@my.waketech.edu>
2025-11-06 08:42:29 -0500

    Update BoohbahRebellion.java

--- BoohbahRebellion.java
+++ BoohbahRebellion.java
@@ -1,11 +1,21 @@
 public class SupremeBoohbah {
     protected String name;
 
-    public ZingZingZingbah() {
+    public SupremeBoohbah() {
+        name = "ZingZingZingbah";
         System.out.println("The SupremeBoohbah has awakened");
     }
 
-    public String void command() {
+    public void command() {
         System.out.println("Cower in fear before your new leader.");
+    }
+
+    public void command(String message) {
+        System.out.println(message);
+    }
+
+    @Override
+    public String toString() {
+        return "SupremeBoohbah named " + name;
     }
 }
