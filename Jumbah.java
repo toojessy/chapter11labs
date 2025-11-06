e57db7390d5c4562512390a33210a50533218af2
toojessy <jnkamienski@my.waketech.edu>
2025-11-06 09:45:37 -0500

    Create Jumbah.java

---
+++ Jumbah.java
@@ -0,0 +1,12 @@
+public class Jumbah extends SupremeBoohbah{
+
+    public Humbah() {
+        name = "Jumbah";
+        System.out.println("refuses to submit!");
+    }
+
+    @Override
+    public String toString() {
+        return name + " says: NEVER!!";
+    }
+}
