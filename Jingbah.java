06764036dce41c2325ce2ee7677ee0707f39d916
toojessy <jnkamienski@my.waketech.edu>
2025-11-06 09:45:47 -0500

    Create Jingbah.java

---
+++ Jingbah.java
@@ -0,0 +1,12 @@
+public class Jingbah extends SupremeBoohbah{
+
+    public Jingbah() {
+        name = "Jingbah";
+        System.out.println("want peace between all Boohbah");
+    }
+
+    @Override
+    public String toString() {
+        return name + " says: lets all be friends.";
+    }
+}
