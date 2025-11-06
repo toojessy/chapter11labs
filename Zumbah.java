28c8569da6b10be1e57f49779a668d83758abaa5
toojessy <jnkamienski@my.waketech.edu>
2025-11-06 09:45:23 -0500

    Create Zumbah.java

---
+++ Zumbah.java
@@ -0,0 +1,12 @@
+public class Zumbah extends SupremeBoohbah{
+
+    public Zumbah() {
+        name = "Zumbah";
+        System.out.println("proclaims loyalty to Zing Zing Zingbah with crossed fingers behind back.");
+    }
+
+    @Override
+    public String toString() {
+        return name + "  pretends to conform, but initiates a POWER HUM.";
+    }
+}
