9fea62da017ba18481d2bc0b022081aac064a2bd
toojessy <jnkamienski@my.waketech.edu>
2025-11-07 09:44:48 -0500

    Update Zumbah.java

--- Zumbah.java
+++ Zumbah.java
@@ -3,13 +3,13 @@ public class Zumbah extends SupremeBoohbah{
     public Zumbah() {
         super();
         name = "Zumbah";
-        System.out.println("proclaims loyalty to ZingZingZingbah with crossed fingers behind back.");
+        System.out.println(name + " proclaims loyalty to Zing Zing Zingbah with crossed fingers behind back.");
     }
 
     @Override
-    public void command(String message) {
-        String m = " proclaims loyalty to ZingZingZingbah with crossed fingers behind back.";
-        System.out.println(message + m);
+    public void command() {
+        System.out.println(name + "  pretends to conform, but initiates a POWER HUM.");
+
     }
 
     @Override
