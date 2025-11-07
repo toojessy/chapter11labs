6fd25a2ae6685d79c3dc94f732db00dd5432347c
toojessy <jnkamienski@my.waketech.edu>
2025-11-06 22:32:42 -0500

    Update BoohbahRebellionTest.java

--- BoohbahRebellionTest.java
+++ BoohbahRebellionTest.java
@@ -1,11 +1,26 @@
 public class BoohbahRebellionTest {
+
     public static void main(String[] args) {
         SupremeBoohbah[] boohbahs = new SupremeBoohbah[4];
+        Humbah humbah = new Humbah();
+        Jingbah jingbah = new Jingbah();
+        Zumbah zumbah = new Zumbah();
+        Jumbah jumbah = new Jumbah();
+
+        boohbahs[0] = humbah;
+        boohbahs[1] = jingbah;
+        boohbahs[2] = zumbah;
+        boohbahs[3] = jumbah;
+
         System.out.println(" --- Boohbah Rebellion Begins ---");
-        // code
+        for (int i = 0; i < boohbahs.length; i++) {
+            boohbahs[i].command();
+        }
 
         System.out.println(" --- Overloaded Commands ---");
-        //code
-        
+        boohbahs[2].command("pretends to conform, but initiates a POWER HUM.");
+        boohbahs[1].command("calms everyone down.", true);
+       // boohbahs[1].command("Loudly calms everyone down.", false);
+
     }
 }
