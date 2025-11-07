15f6af9a9eb73ac3c520cd99ee66bb7b2fa696f4
toojessy <jnkamienski@my.waketech.edu>
2025-11-07 09:45:37 -0500

    Update Humbah.java

--- Humbah.java
+++ Humbah.java
@@ -3,11 +3,11 @@ public class Humbah extends SupremeBoohbah{
     public Humbah() {
         super();
         name = "Humbah";
-        System.out.println(name + "proclaims loyalty to Zing Zing Zingbah.");
+        System.out.println(name + " proclaims loyalty to Zing Zing Zingbah.");
     }
 
     @Override
-    public String toString() {
-        return name + " follows orders without question.";
+    public void command() {
+        System.out.println(name + " follows orders without question.");
     }
 }
