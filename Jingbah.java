c6dc2868157714659518e39db5d3aea1b8a1c13b
toojessy <jnkamienski@my.waketech.edu>
2025-11-06 22:30:27 -0500

    Update Jingbah.java

--- Jingbah.java
+++ Jingbah.java
@@ -1,9 +1,26 @@
 public class Jingbah extends SupremeBoohbah{
 
     public Jingbah() {
+        super();
         name = "Jingbah";
         System.out.println("want peace between all Boohbah");
     }
+
+    @Override
+    public void command(String message) {
+        String m = "want peace between all Boohbah";
+        System.out.println(message + m);
+    }
+
+    public void command(String message, boolean whisper) {
+        if (whisper) {
+            System.out.println("Quietly calms " + message);
+        }
+        else {
+            System.out.println("Loudly calms " + message);
+        }
+
+}
 
     @Override
     public String toString() {
