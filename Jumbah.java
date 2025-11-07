0200ba8417ff61007e062f7e481a8ad3d718551c
toojessy <jnkamienski@my.waketech.edu>
2025-11-06 22:31:05 -0500

    Update Jumbah.java

--- Jumbah.java
+++ Jumbah.java
@@ -1,12 +1,15 @@
 public class Jumbah extends SupremeBoohbah{
 
-    public Humbah() {
+    public Jumbah() {
+        super();
         name = "Jumbah";
         System.out.println("refuses to submit!");
     }
 
     @Override
     public String toString() {
+        String message = name + " says: NEVER!!";
+        super.command(message);
         return name + " says: NEVER!!";
     }
 }
